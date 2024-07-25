7b7124d9f656bc7b3e07cbf2138720c53e4fdf22
edolubis-ta <edo.lubis@tentanganak.id>
2024-07-25 18:09:06 +0700

    Initial Commit

---
+++ HeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroApp
+//
+//  Created by edo lubis on 25/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HeroAppUITests/HeroAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HeroAppUITestsLaunchTests.swift
+//  HeroAppUITests
+//
+//  Created by edo lubis on 25/07/24.
+//
+
+import XCTest
+
+final class HeroAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
